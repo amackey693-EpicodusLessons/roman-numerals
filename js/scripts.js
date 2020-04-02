92b7cd120be73c5266715fa6dc30fa0d0ba851c3
Mark Gridley <mark_gridley@yahoo.com>
2020-04-02 11:28:21 -0700

    experiment with for statements

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,39 @@ var numeralOnes = ["", "I", "II", "III","IV", "V", "VI", "VII", "VIII", "IX"];
 var numeralTens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]; 
 var numeralHundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "CM", "M"]; // Hundreds & 1000
 
+//ones
 for (var i=0; i < numeralOnes.length; i += 1) {
-  ones += numeralOnes[i];
-  console.log(ones)
+  ones = numeralOnes[i];
+  // console.log(ones)
 }
 
-for (var ii=0; ii< numeralTens.length; ii += 1) {
-  tens += numeralTens[ii];
-  console.log(tens)
+//tens
+for (var i=0; i< numeralTens.length; i += 1) {
+  tens = numeralTens[i];
+  // console.log(tens)
 }
 
-for (var iii=0; iii < numeralHundreds.length; iii += 1) {
-  hundreds += numeralHundreds[iii];
-  console.log(hundreds)
+//hundreds
+for (var i=0; i < numeralHundreds.length; i += 1) {
+  hundreds = numeralHundreds[i];
+  // console.log(hundreds)
 }
+
+var numeralConverter = function (userInput) {
+//   if (userInput <= 10) {
+//     return ones === userInput;
+//     console.log(userInput)
+//   } 
+
+// }
+
+  if (numeralOnes.includes(userInput)) {
+    userInput = (userInput === numeralOnes);
+    console.log(userInput)
+  }
+}
+
+
 
 
 // var total = 0;
@@ -29,17 +48,13 @@ for (var iii=0; iii < numeralHundreds.length; iii += 1) {
 //   console.log(total)
 
 
-
 // User Interface Logic : 
 $ (document).ready(function(){
   $("form#numerals").submit(function(event){
     event.preventDefault();
     var userInput = parseInt($("#userInput").val());
-    var romanNumeral = [];
-
-    // console.log(userInput)
+    var romanNumeral = []
 
     $("#result").show(romanNumeral);
   });
 });
-
