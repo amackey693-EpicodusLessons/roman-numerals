1ac5bf65308b261d5694c7d583912644903da900
Mark Gridley <mark_gridley@yahoo.com>
2020-04-02 16:18:28 -0700

    remove excess commented code, reduce size of numeralNumbers var

--- js/scripts.js
+++ js/scripts.js
@@ -1,108 +1,39 @@
 // Business Logic : 
 
-var ones = "";
-var tens = "";
-var hundreds = "";
-var thousands = "";
-var numeralOnes = ["I", "II", "III","IV", "V", "VI", "VII", "VIII", "IX"]; 
-var numeralTens = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]; 
-var numeralHundreds = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "CM"];
-var numeralThousands = ["M", "MM", "MMM"];
-
-//ones
-for (var i=0; i < numeralOnes.length; i += 1) {
-  ones = numeralOnes[i];
-  // console.log(ones)
-}
-//tens
-for (var i=0; i< numeralTens.length; i += 1) {
-  tens = numeralTens[i];
-    
-  // console.log(tens)
-}
-
-//hundreds
-for (var i=0; i < numeralHundreds.length; i += 1) {
-  hundreds = numeralHundreds[i];
-  // console.log(hundreds)
-}
-
-//thousands
-for (var i=0; i< numeralThousands.length; i += 1) {
-  thousands = numeralThousands[i];
-  // console.log(thousands)
-}
-
-// var numeralConverter = function (converter) {
-//   if (userInput === "1") {
-//     return ones[0].replace(userInput);
-//     // console.log(userInput)
-//   } 
-// }
-
-//   if (numeralOnes.includes(userInput)) {
-//     userInput = (userInput === numeralOnes);
-//     console.log(userInput)
-//   }
-// }
-
-
-
-
-  
-
-
-
-  
-
-
-
-
+var numeralNumbers = [
+  [3000, "MMM"], [2000, "MM"], [1000, "M"], [900, "CM"], [500, "D"], [400, "CD"], [100, "C"], [90,"XC"], [50,"L"], [40,"XL"],[10,"X"], [9,"IX"], [5, "V"], [4,"IV"], [1, "I"] 
+];
+var toRomanNumeral = function(userInput) {
+  if (userInput === 0) {
+    return ' ';
+  }
+  for (var i=0; i < numeralNumbers.length; i++) {
+    if (userInput >= numeralNumbers[i][0]) {
+      return numeralNumbers[i][1] + toRomanNumeral (userInput - numeralNumbers[i][0]);
+    }
+  }
+};
   // User Interface Logic : 
 $ (document).ready(function(){
   $("form#numerals").submit(function(event){
     event.preventDefault();
-    var userInput = $("#userInput").val().split() //an array of single digit strings
-    // var userInput = parseInt($("#userInput").val()) 
+    // var userInput = $("#userInput").val().split() //an array of single digit strings
+    var userInput = parseInt($("#userInput").val());
+    var newNumber = toRomanNumeral(userInput);
+
+    console.log(newNumber)
+    console.log(userInput)
 
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
   
 
-    console.log(userInput)
+    // console.log(userInput)
     // get user input
     // split it into characters (1s 10s 100s, etc)
     // run one function on each character
     // concatenate them all together and into the DOM
 
-    
-        var arrString = string.split(" ");
-      arrString.forEach(function(romans){
 
-        if (numeralOnes.includes(userInput)) {
-           romans = ;
-        }
-
-        else if (numeralOnes.includes(userInput)) && (numeralTens.includes(userInput)) {
-          romans = ; 
-        }
-      })
-
-
-    // var arrString = string.split(" ");
-    //     arrString.forEach(function(word){
-          
-    //       // pulls words beginning with vowels and adds "way" at the end
-
-    //       if (vowels.includes(word[0])) {
-    //         word = word + "way";
-    //         pig.push(word);
-    //       }
-
-    //       else if (word[0] === "q" && word[1] === "u") {
-    //         var qWord = word.toString();
-    //         var qWord2 =qWord.substr(2);
-    //         pig.push(qWord2+"quway");
-    //       }
-    $("#result").show();
+    $("#result").append(newNumber);
   });
 });
